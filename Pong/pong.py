54c01bc41a5c7f2055eb9ffb6bbd8f1acdc74992
Alezyzz <alexander.bjornas@gmail.com>
2022-06-16 17:03:37 +0200

    collision somewhat fixed

--- Pong/pong.py
+++ Pong/pong.py
@@ -72,8 +72,22 @@ def draw(win, paddles, ball):
     
     # Drawing the ball
     ball.draw(win)
-
     pygame.display.update()
+
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
 
 # Movement keys of the paddles
 def handle_paddle_movement(keys, left_paddle, right_paddle):
@@ -113,6 +127,7 @@ def main():
         handle_paddle_movement(keys, left_paddle, right_paddle)
 
         ball.move()
+        handle_collision(ball, left_paddle, right_paddle)
 
 
 
